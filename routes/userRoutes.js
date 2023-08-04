fa4465f4c12c9c43c95b7e55e6ea8572c36cec03
“Lissa <lwarshaw674@gmail.com>
2023-08-04 08:10:20 -0700

    user routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import * as controllers from "../controllers/userController.js";
+
+const router = Router();
+
+// Sign up a new user
+router.post("/signup", controllers.signUp);
+
+// Sign in a user
+router.post("/signin", controllers.signIn);
+
+// Update an existing user
+router.put("/:id", controllers.updateUser);
+
+// Delete a user
+router.delete("/:id", controllers.deleteUser);
+
+export default router;
